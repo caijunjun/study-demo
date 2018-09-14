af07dadd5d66bf43c41add5e639ce6a034f49b77
caijunjun <caijunjunit@163.com>
2018-09-14 20:06:37 +0800

    spring cloud gateway  logback优化

---
+++ study-web-2.0/web-gateway-2.0/src/main/java/com/web/gateway/Application.java
@@ -0,0 +1,14 @@
+package com.web.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//服务注册发现
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ study-web-2.0/web-gateway-2.0/src/main/java/com/web/gateway/config/KeyResolverConfiguration.java
@@ -0,0 +1,41 @@
+package com.web.gateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class KeyResolverConfiguration {
+
+	/**
+	 * ip限流
+	 * @return
+	 */
+	@Bean
+	public KeyResolver ipKeyResolver() {
+		return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
+	}
+
+	/**
+	 * 用户限流（必须携带相应参数）
+	 * @return
+	 */
+	@Bean
+	KeyResolver userKeyResolver() {
+		return exchange -> Mono.just(exchange.getRequest().getQueryParams().getFirst("userId"));
+	}
+
+	
+	/**
+	 * 接口限流
+	 * @return
+	 */
+	@Bean
+	KeyResolver apiKeyResolver() {
+		return exchange -> Mono.just(exchange.getRequest().getPath().value());
+	}
+	
+	
+}
